12ebe002ed6e177bf32eff7779ec1ac5983c7dbc
dejvidEm <bithug10@gmail.com>
2025-06-03 16:31:51 +0200

    fix 4

--- components/footer.tsx
+++ components/footer.tsx
@@ -90,7 +90,7 @@ export default function Footer() {
         <div className="container mx-auto px-4 py-8">
           <div className="flex flex-col md:flex-row justify-between items-center">
             <p className="text-gray-400 text-sm">
-              &copy; {new Date().getFullYear()} EuroKonstrukt. Všetky práva vyhradené.
+              &copy; {new Date().getFullYear()} 21CBC s.r.o. Všetky práva vyhradené.
             </p>
             <div className="mt-4 md:mt-0">
               <ul className="flex space-x-8">
